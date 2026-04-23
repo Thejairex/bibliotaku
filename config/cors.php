e748aafd4a5ee75f4df296508349615b7909a3c4
Thejairex <yairjesus777@gmail.com>
2026-04-22 21:39:17 -0300

    feat: configure CORS middleware and origins in the application bootstrap

---
+++ config/cors.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'paths' => ['api/*', 'oauth/*', 'sanctum/csrf-cookie'],
+    'allowed_methods' => ['*'],
+    'allowed_origins' => [
+        'https://bibliotaku.thejairex.dpdns.org',
+    ],
+    'allowed_origins_patterns' => [
+        '#^http://localhost:\d+$#',
+        '#^http://127\.0\.0\.1:\d+$#',
+    ],
+    'allowed_headers' => ['*'],
+    'exposed_headers' => [],
+    'max_age' => 0,
+    'supports_credentials' => false,
+];
