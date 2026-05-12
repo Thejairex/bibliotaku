0a921d592a7d13b493e7fb09599a25a97dff28bb
Thejairex <yairjesus777@gmail.com>
2026-05-12 13:02:30 -0300

    refactor: update Vite configuration for improved file refresh handling and add external CSS sources

--- vite.config.js
+++ vite.config.js
@@ -8,10 +8,24 @@ export default defineConfig({
     plugins: [
         laravel({
             input: ['resources/css/app.css', 'resources/js/app.js'],
-            refresh: true,
+            refresh: [
+                'resources/views/**/*.blade.php',
+                'resources/views/*.blade.php',
+                'app/Livewire/**/*.php',
+            ],
         }),
         tailwindcss(),
     ],
+    css: {
+        external: [
+            /^https:\/\/fonts\.googleapis\.com/,
+            /^https:\/\/fonts\.gstatic\.com/,
+        ],
+    },
+    // build: {
+    //     reportCompressedSize: false,
+    //     chunkSizeWarningLimit: 2000,
+    // },
     server: {
         cors: true,
         watch: {
